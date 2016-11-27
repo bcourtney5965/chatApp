ee1a253516c50a776cb8c2f2f1b2bf7775110a15
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-11-26 16:16:19 -0800

    Front end project scaffolding

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('../client'))
+
 app.get('/', function (req, res) {
   console.log('test');
   res.json({ message: 'Welcome to the myChatApp\'s RESTful API!'});
