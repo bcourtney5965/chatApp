c802fa7a980bceaa422df3338f1ad1ac59a9e456
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-11-27 19:30:29 -0800

    Swapped out client with a seed repo and created signup view

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('../client'))
+app.use(express.static('../client/app'))
 
 app.get('/', function (req, res) {
   console.log('test');
